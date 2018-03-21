b92b8861aa3af76d13111edae6b29e3475435eb3
meganii <yuhei24@gmail.com>
2018-03-21 15:51:36 +0900

    change library

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,84 +1,16 @@
 <template>
   <div>
-    <vue-highcharts :options="options" ref="lineCharts"></vue-highcharts>
-    <button @click="load">load</button>
+    <commit-chart :width="900" :height="300"></commit-chart>
   </div>
 </template>
 
 <script>
-  import VueHighcharts from 'vue2-highcharts'
-  const asyncData = {
-    name: 'Tokyo',
-    marker: {
-      symbol: 'square'
-    },
-    data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, {
-      y: 26.5,
-      marker: {
-        symbol: 'url(http://www.highcharts.com/demo/gfx/sun.png)'
-      }
-    }, 23.3, 18.3, 13.9, 9.6]
+import CommitChart from './LandingPage/CommitChart'
+export default {
+  components: {
+    CommitChart
   }
-
-  export default {
-    name: 'landing-page',
-    components: { VueHighcharts },
-    data () {
-      return {
-        options: {
-          chart: {
-            type: 'spline'
-          },
-          title: {
-            text: 'Monthly Average Temperature'
-          },
-          xAxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-          },
-          yAxis: {
-            title: {
-              text: 'Temperature'
-            },
-            labels: {
-              formatter: function () {
-                return this.value + '°'
-              }
-            },
-            tooltip: {
-              crosshairs: true,
-              shared: true
-            },
-            credits: {
-              enabled: true
-            },
-            plotOptions: {
-              spline: {
-                marker: {
-                  radius: 4,
-                  lineColor: '#666666',
-                  lineWidth: 1
-                }
-              }
-            },
-            series: []
-          }
-        }
-      }
-    },
-    methods: {
-      load () {
-        const lineCharts = this.$refs.lineCharts
-        lineCharts.delegateMethod('showLoading', 'Loading....')
-        setTimeout(_ => {
-          lineCharts.addSeries(asyncData)
-          lineCharts.hideLoading()
-        })
-      },
-      open (link) {
-        this.$electron.shell.openExternal(link)
-      }
-    }
-  }
+}
 </script>
 
 <style>
